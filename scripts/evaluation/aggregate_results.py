abca14b93845e01099ebb0260a26536eee733aab
Nirmalie Perera <nirmalieshehani27@gmail.com>
2026-06-08 16:35:04 +0530

    Aggragated results after enhacement of year extraction

--- scripts/evaluation/aggregate_results.py
+++ scripts/evaluation/aggregate_results.py
@@ -22,12 +22,20 @@ PROJECT_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR, "..", ".."))
 #    METHOD
 #)
 
+#EVAL_DIR = os.path.join(
+#    PROJECT_ROOT,
+#    "output_data",
+#    "Evaluation_Output",
+#    "evaluations",
+#    "grobid_enhanced_after_evaluation_modify"
+#)
+
 EVAL_DIR = os.path.join(
     PROJECT_ROOT,
     "output_data",
     "Evaluation_Output",
     "evaluations",
-    "grobid_enhanced_after_evaluation_modify"
+    "grobid_after_year_enhance"
 )
 
 #OUTPUT_PATH = os.path.join(
